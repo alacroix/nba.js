edc0c9671ff61296e14628110c9786c3b642604b
Adrien Lacroix <dev@lx-consulting.fr>
2019-02-11 13:57:13 +0100

    :sparkles: add proxy usage if defined

--- src/api/stats/utils/fetch.js
+++ src/api/stats/utils/fetch.js
@@ -2,8 +2,11 @@ import got from "got";
 
 import { URL } from "./../constants";
 import UserAgent from "user-agents";
+import tunnel from "tunnel";
 
 const randomUserAgent = new UserAgent({ deviceCategory: "desktop" });
+
+const { HTTPS_PROXY_HOST, HTTPS_PROXY_PORT } = process.env;
 
 /**
  * Make a request to the stats API and return an error-first callback with the
@@ -16,6 +19,14 @@ const randomUserAgent = new UserAgent({ deviceCategory: "desktop" });
 export default function fetch(endpoint = "", opts) {
   opts = Object.assign(
     {
+      ...(HTTPS_PROXY_HOST && {
+        agent: tunnel.httpsOverHttp({
+          proxy: {
+            host: HTTPS_PROXY_HOST,
+            port: HTTPS_PROXY_PORT || 80
+          }
+        })
+      }),
       headers: {
         "accept-encoding": "Accepflate, sdch",
         "accept-language": "he-IL,he;q=0.8,en-US;q=0.6,en;q=0.4",
